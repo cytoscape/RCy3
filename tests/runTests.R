cad0813e6bda26ce5b3ad2d5064b0117a80086c0
Tanja <tanja.muetze14@imperial.ac.uk>
2015-10-02 16:49:30 +0200

    Paul’s update to v 24
    Former-commit-id: ff30d53e29f4b5ae24e6a14e869ec5ab23e2b5bf [formerly 3f6634262b270fb503169b5117aa21c3aa042db3]
    Former-commit-id: c26b22d9b3052ffca9fc2559a1b064cfa1a81bfe
    Former-commit-id: d0eb64866bc3b72f4b603b8c3c212c52e5405bdb

--- tests/runTests.R
+++ tests/runTests.R
@@ -1,2 +1,3 @@
-require(RCy3) || stop("unable to load RCy3")
-BiocGenerics:::testPackage('RCy3')
+# disabled in the build systems, takes too long to run!  (pshannon, 1 oct 2015)
+#require(RCy3) || stop("unable to load RCy3")
+#BiocGenerics:::testPackage('RCy3')
