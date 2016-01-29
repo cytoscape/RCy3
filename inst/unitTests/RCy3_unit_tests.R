2ddb87f5e6b675eb734bf6e466bd51e09a7eaf7e
Tanja <tmuetze@users.noreply.github.com>
2016-01-28 17:25:41 -0800

    Update RCy3_unit_tests.R
    Former-commit-id: db22a3646f1cc01c597c93b373b27785986471f2 [formerly cb50fcb75824b25cf52e554610086558bdf78770]
    Former-commit-id: a2a457931d67ef47695ae5ac268cf98bdf3f9966
    Former-commit-id: 1bd4a7069d15df3c8a9dec1bf622a216e085a461

--- inst/unitTests/RCy3_unit_tests.R
+++ inst/unitTests/RCy3_unit_tests.R
@@ -1,3 +1,3 @@
-print ("Automatic execution of RCytoscape unit tests disabled because they take too long for the build machine.")
-#require("RCy3") || stop("unable to load RCytoscape package")
+print ("Automatic execution of RCy3 unit tests disabled because they take too long for the build machine.")
+#require("RCy3") || stop("unable to load RCy3 package")
 #RCy3:::.test()
