291adae82e07435cab2b8d98c63a8ffdeecb0bee
Alexander Pico <apico@gladstone.ucsf.edu>
2018-01-14 13:32:20 -0800

    another r2cytoscape vignette cleared

--- R/RCy3.R
+++ R/RCy3.R
@@ -5846,10 +5846,10 @@ createNetworkFromDataFrames <- function(nodes=NULL,edges=NULL,network.name="MyNe
         return(response)
     
     cat("Applying default style\n")
-    commandRun('vizmap apply styles="default"')
+    commandRun('vizmap apply styles="default"',obj)
     
     cat(sprintf("Applying %s layout\n", invisible(commandRun('layout get preferred network="current"'))))
-    commandRun('layout apply preferred networkSelected="current')
+    commandRun('layout apply preferred networkSelected="current',obj)
     
     return(network.suid)
 }
@@ -6120,6 +6120,174 @@ getNetworkName <- function(network.suid=NA, obj=CytoscapeConnection()){
     network.name <- unname(fromJSON(rawToChar(response$content)))
     network.name <- network.name[[1]]$name
     return(network.name)
+}
+
+# ------------------------------------------------------------------------------
+#' Get table column values
+#'
+#' @details The 'name' column is always retrieved along with specified columns. The 'name' values are used
+#' as row names in the returned data frame. Note that this function fails on columns with missing values.
+#' @param table name of table, e.g., node, edge, network
+#' @param columns names of columns to retrieve values from as list object or comma-separated list; default is all columns
+#' @param namespace namespace of table; default is "default"
+#' @param network name or suid of the network; default is "current" network
+#' @param base.url cyrest base url for communicating with cytoscape
+#' @return data frame of column values
+#' @export
+#' @import httr
+#' @import RJSONIO
+#' @importFrom utils URLencode
+#' @examples
+#' \donttest{
+#' example(createNetwork)
+#'
+#' getTableColumns('node','group')
+#' }
+
+getTableColumns<-function(table,columns=NULL,namespace='default',obj=CytoscapeWindowFromNetwork()){
+    
+    base.url = paste(obj@uri,obj@api,sep="/")
+    
+    #all columns
+    if(is.null(columns))
+        columns = listTableColumns(table, namespace, obj=obj)
+    
+    #handle comma separated lists and list objects
+    col.list = columns
+    if(length(col.list)==1)
+        col.list = unlist(strsplit(columns, ","))
+    
+    #get name column first
+    tbl = paste0(namespace,table)
+    cmd = paste(base.url,'networks',obj@suid,'tables',tbl,'columns','name',sep = '/')
+    res = GET(URLencode(cmd))
+    res.html = htmlParse(rawToChar(res$content), asText=TRUE)
+    res.elem = xpathSApply(res.html, "//p", xmlValue)
+    names <- fromJSON(res.elem[1])
+    df = data.frame(row.names=names$values)
+    
+    #retrieve column names
+    table.col.list = listTableColumns(table,namespace,obj)
+    
+    # then append other requested columns
+    for (col in col.list){
+        
+        #check for column names
+        if(!col %in% table.col.list){
+            cat(sprintf("Error: Column %s not found in %s table \n",col,table))
+            next()
+        }
+        
+        cmd = paste(base.url,'networks',obj@suid,'tables',tbl,'columns',col,sep = '/')
+        res = GET(URLencode(cmd))
+        res.html = htmlParse(rawToChar(res$content), asText=TRUE)
+        res.elem = xpathSApply(res.html, "//p", xmlValue)
+        col.vals <- fromJSON(res.elem[1])
+        #convert NULL to NA, then unlist
+        cvv <- unlist(lapply(col.vals$values, function(x) ifelse(is.null(x),NA,x)))
+        if(length(names$values)==length(cvv)){
+            for(i in 1:length(names$values)){
+                df[i,col] <- cvv[i]
+            }
+        } else {
+            print("Warning: Requested column has missing values. Returning single column without row.names...")
+            df2 = data.frame(col=unlist(col.vals$values))
+            return(df2)
+        }
+    }
+    
+    return(df)
+}
+
+# ------------------------------------------------------------------------------
+#' List names of all columns in a table
+#'
+#' @param table name of table, e.g., node, edge, network; default is "node"
+#' @param namespace namespace of table, e.g., default
+#' @param network name or suid of the network; default is "current" network
+#' @param base.url cyrest base url for communicating with cytoscape
+#' @return list of column names
+#' @export
+#' @import httr
+#' @import RJSONIO
+#' @examples
+#' \donttest{
+#' listTableColumns()
+#' listTableColumns('edge')
+#' listTableColumns('network')
+#' }
+
+listTableColumns<-function(table='node',namespace='default',obj=CytoscapeWindowFromNetwork()){
+    base.url = paste(obj@uri,obj@api,sep="/")
+    cmd = paste(table,' list attributes network="',obj@title,'" namespace="',namespace,sep='')
+    return(commandRun(cmd,obj))
+}
+
+# ------------------------------------------------------------------------------
+#' Loads data into Cytoscape tables keyed by row
+#'
+#' @description This function loads data into Cytoscape node/edge/network tables provided a
+#' common key, e.g., name. Data.frame column names will be used to set Cytoscape table column
+#' names.
+#' @details Numeric (or integer) values will be stored as Doubles in Cytoscape tables.
+#' Character or mixed values will be stored as Strings. Logical values will be stored as Boolean.
+#' Existing columns with the same names will be overwritten.
+#' @param data (data.frame) each row is a node and columns contain node attributes
+#' @param data.key.column (char) name of data.frame column to use as key; default is "row.names"
+#' @param table (char) name of Cytoscape table to load data into, e.g., node, edge or network; default is "node"
+#' @param table.key.column (char) name of Cytoscape table column to use as key; default is "name"
+#' @param network name or suid of the network; default is "current" network
+#' @param base.url cyrest base url for communicating with cytoscape
+#' @return server response
+#' @export
+
+loadTableData<-function(data, data.key.column='row.names', table='node', table.key.column='name',
+                        obj=CytoscapeWindowFromNetwork()){
+    
+    base.url = paste(obj@uri,obj@api,sep="/")
+    
+    table.key.column.values =  getTableColumns(table=table,columns = table.key.column, obj=obj)
+    
+    ##ERROR if table.key.column.values is empty
+    if(ncol(table.key.column.values)==0)
+        return("Failed to load data: Please check table.key.column")
+    
+    if(data.key.column=='row.names')  # if key in row.names...
+        data$row.names<-row.names(data)  # then copy to new "row.names" column :)
+    
+    ##ERROR if data.key.column not in data
+    if (!data.key.column %in% colnames(data))
+        return("Failed to load data: Please check data.key.column")
+    
+    filter = data[,data.key.column] %in% table.key.column.values[,1]
+    
+    ##ERROR if filter is empty
+    if(!TRUE %in% filter)
+        return("Failed to load data: Provided key columns do not contain any matches")
+    
+    data.subset = data[filter,]
+    
+    #remove troublesome factors
+    if(class(data.subset[,data.key.column])=="factor")
+        data.subset[,data.key.column] = levels(droplevels(data.subset[,data.key.column]))
+    
+    data.list <- c()
+    for(i in 1:dim(data.subset)[1]){  #each rows
+        rest <- list()
+        for(j in 1:dim(data.subset)[2]){  #each column
+            rest[[colnames(data.subset)[j]]] = data.subset[i,j]
+        }
+        data.list[[i]] <- rest
+    }
+    
+    table = paste0("default",table) #add prefix
+    
+    tojson.list <- list(key=table.key.column,dataKey=data.key.column,data=data.list)
+    jsonData <- toJSON(tojson.list)
+    data.url <- paste(base.url,'networks',obj@suid,"tables",table, sep = '/')
+    
+    PUT(url=data.url,body=jsonData, encode="json")
+    return(sprintf("Success: Data loaded in %s table", table))
 }
 
 # ------------------------------------------------------------------------------
