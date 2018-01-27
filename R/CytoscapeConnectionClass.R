330673c54a55e5c4de6d980c6ec412d1a6083eea
Alexander Pico <apico@gladstone.ucsf.edu>
2018-01-26 16:18:39 -0800

    fixed getFirstNeighbors; cont docs and sep
    Former-commit-id: 536027657df6975f2a14d3daeb4edadfdf301871 [formerly b13d90010cf0c6406e253f0cd5436778bc52c80f]
    Former-commit-id: 1904338dfdc9fe621deb653a4bb6e87a5fb39e36
    Former-commit-id: 84da42b3298048323ba76938c5883b6b0242e539
    Former-commit-id: f0f6d4946f0e5bc7ea37648880c0153c3cc8bfbd

---
+++ R/CytoscapeConnectionClass.R
@@ -0,0 +1,50 @@
+#--------------------------------------------------------------------------------
+# this code is for the Bioconductor build system. You should never need to set or
+# read these environment variables in ordinary use.
+.BBSOverride <- function(host, port) {
+    ret <- list()
+    if ((Sys.getenv("RCYTOSCAPE3_PORT_OVERRIDE") != "") &&  (Sys.getenv("RCYTOSCAPE3_HOST_OVERRIDE") != "")) {
+        host = Sys.getenv("RCYTOSCAPE3_HOST_OVERRIDE")
+        port = as(Sys.getenv("RCYTOSCAPE3_PORT_OVERRIDE"),"integer")
+    }
+    if (.Platform$r_arch == "x64") {
+        if ((Sys.getenv("RCYTOSCAPE3_PORT_OVERRIDE_64") != "") &&  (Sys.getenv("RCYTOSCAPE3_HOST_OVERRIDE_64") != "")) {
+            host = Sys.getenv("RCYTOSCAPE3_HOST_OVERRIDE_64")
+            port = as(Sys.getenv("RCYTOSCAPE3_PORT_OVERRIDE_64"),"integer")
+        }
+    }
+    #cat(paste("Using host", host, "and port", port, "."))
+    
+    ret["host"] <- host
+    ret["port"] <- port
+    ret
+}
+
+
+# ------------------------------------------------------------------------------
+printf = function (...) print (noquote (sprintf (...)))
+
+# ------------------------------------------------------------------------------
+setClass("CytoscapeConnectionClass", 
+         slots = c(uri="character",api="character"), 
+         prototype = prototype(uri="http://localhost:1234", api="v1")
+)
+# Constructor
+CytoscapeConnection = function(host='localhost', port=1234) {
+    res <- .BBSOverride(host, port)
+    host = res$host
+    port = res$port
+    uri = sprintf('http://%s:%s', host, port)
+    cc = new('CytoscapeConnectionClass', uri = uri)
+    if (!url.exists(uri)){
+        write(sprintf('Connection failed.'), stderr())
+        write(sprintf('To troubleshoot: 1) Please ensure that you have Cytoscape open'), stderr())
+        write(sprintf('2) that the latest version of CyREST is installed.'), stderr())
+        write(sprintf('3) that Cytoscape uses Java 8 (not 7 or below).'), stderr())
+        write(sprintf('To help troubleshooting, please check:'), stderr())
+        write(sprintf('http://www.cytoscape.org/troubleshooting.html'), stderr())
+        return()
+    }
+    cc@api = apiVersion(cc)
+    return(cc)
+} # END CytoscapeConnection
