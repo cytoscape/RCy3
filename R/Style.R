1d633dbf9bbcf5e270ba87ac436ecf60a549a94c
Alexander Pico <apico@gladstone.ucsf.edu>
2018-02-04 14:39:29 -0800

    Main Styles done; added StyleDeps
    Former-commit-id: 33043003b3fa7d1a2a74163dbac379947b6a9342 [formerly 098d585a45a3f5a5807cb743e1d27be90b9b0bfd]
    Former-commit-id: 23f7fe396a94347e1c5953296d86a14efa90d4bb
    Former-commit-id: 61d8effee83d3d176870e509f2ef581c1246bfa7
    Former-commit-id: 31a06f9a8cc4777ce48f9b2ba7f8cbde08e4c26f

--- R/Style.R
+++ R/Style.R
@@ -1,18 +1,23 @@
-#' @include CytoscapeWindowClass.R CytoscapeConnectionClass.R 
+# ------------------------------------------------------------------------------
+#' @export
+copyVisualStyle <- function (from.style, to.style, base.url=.defaultBaseUrl) {
+    current.names = getVisualStyleNames(base.url)
+    if (! from.style %in% current.names){
+        stop (sprintf ('Cannot copy from a non-existent visual style (%s)', from.style))
+    }
+    # get the current style from Cytoscape
+    res <- cyrestGET(paste("styles", from.style, sep="/"), base.url = base.url)
+    style.from.to <- res
+    style.from.to[1] <- as.character(to.style)
+    
+    # and send it to Cytoscape as a new style with a new name
+    cyrestPOST("styles", body = style.from.to, base.url = base.url)
+    
+    # get and update dependencies as well
+    res <- cyrestGET(paste("styles", from.style, "dependencies", sep="/"), base.url = base.url)
+    cyrestPUT(paste("styles", to.style, "dependencies", sep="/"), body=res, base.url = base.url)
+}
 
-#-----------------------------------------------------------
-# methods related to visual styles
-#-----------------------------------------------------------
-setGeneric ('setVisualStyle',       signature='obj', function (obj=CytoscapeWindowFromNetwork(), style.name) standardGeneric ('setVisualStyle'))
-setGeneric ('redraw',				signature='obj', function (obj=CytoscapeWindowFromNetwork()) standardGeneric ('redraw'))
-setGeneric ('getVisualStyleNames',  signature='obj', function (obj=CytoscapeConnection()) standardGeneric ('getVisualStyleNames'))
-setGeneric ('copyVisualStyle',      signature='obj', function (obj=CytoscapeConnection(), from.style, to.style) standardGeneric ('copyVisualStyle'))
-setGeneric ('lockNodeDimensions',   signature='obj', function (obj=CytoscapeConnection(), new.state, style.name='default') standardGeneric ('lockNodeDimensions'))
-setGeneric ('getLineStyles',        signature='obj', function (obj=CytoscapeConnection()) standardGeneric ('getLineStyles'))
-setGeneric ('getArrowShapes', 	    signature='obj', function (obj=CytoscapeConnection()) standardGeneric ('getArrowShapes'))
-setGeneric ('getNodeShapes',        signature='obj', function (obj=CytoscapeConnection()) standardGeneric ('getNodeShapes'))
-setGeneric ('getDirectlyModifiableVisualProperties',    
-                                    signature='obj', function(obj=CytoscapeConnection(), style.name="default") standardGeneric ('getDirectlyModifiableVisualProperties'))
 
 # ------------------------------------------------------------------------------
 #' Create a visual style from components
@@ -52,9 +57,7 @@ setGeneric ('getDirectlyModifiableVisualProperties',
 #' }
 #' @seealso applyStyle, mapVisualProperty
 
-createVisualStyle <- function(style.name, defaults, mappings, obj=CytoscapeConnection()) {
-    
-    base.url=paste(obj@uri,obj@api,sep = "/")
+createVisualStyle <- function(style.name, defaults, mappings, base.url=.defaultBaseUrl) {
     
     if(missing(mappings))
         mappings <- list()
@@ -72,14 +75,41 @@ createVisualStyle <- function(style.name, defaults, mappings, obj=CytoscapeConne
 }
 
 # ------------------------------------------------------------------------------
-setMethod('redraw', 'OptionalCyWinClass', 
-          function(obj) {
-              net.SUID <- as.character(obj@suid)
-              resource.uri <- paste(obj@uri, obj@api, "apply/styles", "default", net.SUID, sep = "/")
-              request.res <- GET(url=resource.uri)
-              invisible(request.res)
-          }) 
-## END redraw
+#' @export
+getArrowShapes <- function(base.url=.defaultBaseUrl) {
+    res <- cyrestGET(paste("styles/visualproperties/EDGE_TARGET_ARROW_SHAPE/values", sep="/"), base.url = base.url)
+    return(res$values)
+}
+
+#------------------------------------------------------------------------------------------------------------------------
+#' @export
+getLineStyles <- function(base.url=.defaultBaseUrl) {
+    res <- cyrestGET(paste("styles/visualproperties/EDGE_LINE_TYPE/values", sep="/"), base.url = base.url)
+    return(res$values)
+}
+
+#------------------------------------------------------------------------------------------------------------------------
+#' @export
+getNodeShapes <- function (base.url=.defaultBaseUrl) {
+    res <- cyrestGET(paste("styles/visualproperties/NODE_SHAPE/values", sep="/"),base.url=base.url)
+    return(res$values)
+}
+
+#------------------------------------------------------------------------------------------------------------------------
+#' @export
+getVisualPropertyNames <- function (style.name="default",base.url=.defaultBaseUrl) {
+    res <- cyrestGET(paste("styles", as.character(style.name), "defaults", sep="/"), base.url = base.url)
+    visual.properties <- unname(res[[1]])
+    visual.properties <- sapply(visual.properties, '[[', 1)
+    return(visual.properties)
+}
+
+# ------------------------------------------------------------------------------
+#' @export
+getVisualStyleNames <- function(base.url=.defaultBaseUrl) {
+    res <- cyrestGET("apply/styles",base.url = base.url)
+    return(unname(res))
+}
 
 # ------------------------------------------------------------------------------
 #' Saves the current visual style as a data file
@@ -94,162 +124,23 @@ setMethod('redraw', 'OptionalCyWinClass',
 #' saveVisualStyle('myStyle','JSON')
 #' }
 
-saveVisualStyle<-function(filename,type,obj=CytoscapeConnection()){
-    commandsPOST(paste0('vizmap export options=',type,' OutputFile="',filename,'"'),obj)
+saveVisualStyle<-function(filename,type,base.url=.defaultBaseUrl){
+    commandsPOST(paste0('vizmap export options=',type,' OutputFile="',filename,'"'),base.url = base.url)
 }
-
-
-# ------------------------------------------------------------------------------
-#' Updates the values of dependencies in a style
-#'
-#' @description Updates style dependencies, overriding any prior settings.
-#' @param style.name (char) name for style
-#' @param dependencies (list) a list of style dependencies, see list below. Note:
-#' each dependency is set by a boolean, TRUE or FALSE (T or F)
-#' @param base.url cyrest base url for communicating with cytoscape
-#' @return server response
-#' @examples
-#' \donttest{
-#' updateStyleDependencies("myStyle",list(nodeSizeLocked=TRUE))
-#' }
-#' @export
-#' @section List of Dependencies:
-#' arrowColorMatchesEdge
-#' nodeCustomGraphicsSizeSync
-#' nodeSizeLocked
-
-updateStyleDependencies <- function(style.name,dependencies,obj=CytoscapeConnection()){
-    
-    base.url=paste(obj@uri,obj@api,sep = "/")
-    
-    dep.list <- list()
-    for (i in 1:length(dependencies)) {
-        dep.list[[i]] <- list(visualPropertyDependency=names(dependencies)[i],
-                              enabled=dependencies[[i]])
-    }
-    
-    style.url <- URLencode(paste(base.url,'styles', style.name,'dependencies', sep = '/'))
-    map.body <- toJSON(dep.list)
-    
-    cat("PUT-ing style\n")
-    invisible(PUT(url=style.url,body=map.body, encode="json"))
-    invisible(commandsPOST(paste('vizmap apply styles',style.name,sep='=')))
-}
-
-
-#------------------------------------------------------------------------------------------------------------------------
-setMethod ('getNodeShapes', 'OptionalCyObjClass', function (obj) {
-    resource.uri <- paste(obj@uri, obj@api, "styles/visualproperties/NODE_SHAPE/values", sep="/")
-    request.res <- GET(resource.uri)
-    request.res <- fromJSON(rawToChar(request.res$content))
-    return(request.res$values)
-})
-
-#------------------------------------------------------------------------------------------------------------------------
-setMethod ('getDirectlyModifiableVisualProperties', 'OptionalCyObjClass',
-           function (obj, style.name="default") {
-               resource.uri = paste(obj@uri, obj@api, "styles", as.character(style.name), "defaults", sep="/")
-               request.res = GET(url=resource.uri)
-               visual.properties <- unname(fromJSON(rawToChar(request.res$content))[[1]])
-               visual.properties <- sapply(visual.properties, '[[', 1)
-               return(visual.properties)
-           })
-#------------------------------------------------------------------------------------------------------------------------
-setMethod ('getLineStyles', 'OptionalCyObjClass', function (obj) {
-    resource.uri <- paste(obj@uri, obj@api, "styles/visualproperties/EDGE_LINE_TYPE/values", sep="/")
-    request.res <- GET(resource.uri)
-    request.res <- fromJSON(rawToChar(request.res$content))
-    return(request.res$values)
-})
-
-# ------------------------------------------------------------------------------
-setMethod('getArrowShapes', 'OptionalCyObjClass', 
-          function(obj) {
-              
-              
-              resource.uri <- paste(obj@uri, obj@api, "styles/visualproperties/EDGE_TARGET_ARROW_SHAPE/values", sep="/")
-              # TanjaM: EDGE_SOURCE_ARROW_SHAPE rather than TARGET returns the same results as of April 2015
-              request.res <- GET(resource.uri)
-              request.res <- fromJSON(rawToChar(request.res$content))
-              return(request.res$values)
-          })
-## END getArrowShapes
-
-# ------------------------------------------------------------------------------
-setMethod('getVisualStyleNames', 'OptionalCyObjClass', 
-          function(obj) {
-              resource.uri = paste(obj@uri, obj@api, "apply/styles", sep="/")
-              request.res = GET(url=resource.uri)
-              style.names = unname(fromJSON(rawToChar(request.res$content)))
-              return(style.names)
-          })
-
-# ------------------------------------------------------------------------------
-setMethod('copyVisualStyle', 'OptionalCyObjClass', 
-          function (obj, from.style, to.style) {
-              current.names = getVisualStyleNames (obj)
-              if (! from.style %in% current.names){
-                  stop (sprintf ('Cannot copy from a non-existent visual style (%s)', from.style))
-              }
-              # get the current style from Cytoscape
-              resource.uri <- paste(obj@uri, obj@api, "styles", from.style, sep="/")
-              from.style.JSON <- GET(url=URLencode(resource.uri))
-              from.style <- fromJSON(rawToChar(from.style.JSON$content))
-              from.style[1] <- as.character(to.style)
-              
-              # and send it to Cytoscape as a new style with a new name
-              to.style.JSON <- toJSON(from.style)
-              resource.uri <- paste(obj@uri, obj@api, "styles", sep="/")
-              request.res <- POST(url = resource.uri, body = to.style.JSON, encode = "json")
-              invisible(request.res)
-          })
 
 # ------------------------------------------------------------------------------
 # apply visual style to network
-setMethod('setVisualStyle', 'OptionalCyWinClass', 
-          function(obj, style.name) {
-              
-              net.SUID = as.character(obj@suid)
-              current.names = getVisualStyleNames(obj)
-              # inform user if they want to set style that does not exist 
-              if(!style.name %in% current.names) { 
-                  stop(sprintf('Cannot call setVisualStyle on a non-existent visual style (%s)', style.name))
-              }
-              # apply style
-              resource.uri <- paste(obj@uri, obj@api, "apply/styles", style.name, net.SUID, sep="/")
-              req.res <- GET(url=URLencode(resource.uri))
-              write(sprintf('network visual style has been set to "%s"', style.name), stdout())
-              invisible(req.res)
-          })
+#' @export
+setVisualStyle <- function(style.name, network=NULL, base.url=.defaultBaseUrl) {
+    net.SUID = getNetworkSuid(network)
+    current.names = getVisualStyleNames(base.url)
+    # inform user if they want to set style that does not exist 
+    if(!style.name %in% current.names) { 
+        stop(sprintf('Cannot call setVisualStyle on a non-existent visual style (%s)', style.name))
+    }
+    cyrestGET(paste("apply/styles", style.name, net.SUID, sep="/"), base.url = base.url)
+    write(sprintf('network visual style has been set to "%s"', style.name), stdout())
+}
 
-#------------------------------------------------------------------------------------------------------------------------
-setMethod ('lockNodeDimensions', 'OptionalCyObjClass',
-           
-           function (obj, new.state, style.name='default') {
-               # launch error if visual style name is missing
-               if (! style.name %in% getVisualStyleNames (obj)) {
-                   write (sprintf ('Error in RCy3::lockNodeDimensions. No visual style named "%s"', style.name), stdout ())
-                   return ()
-               }
-               
-               #lock node dimensions
-               resource.uri <- paste(obj@uri, obj@api, "styles", as.character(style.name), "dependencies", sep="/")
-               style <- list(visualPropertyDependency="nodeSizeLocked", enabled = tolower(new.state))
-               style.JSON <- toJSON(list(style))
-               request.res <- PUT(url=resource.uri, body=style.JSON, encode="json")
-               
-               # inform the user if the request was a success or failure
-               if (request.res$status == 204){
-                   if (new.state==TRUE){
-                       write (sprintf ('Locked node dimensions successfully even if the check box is not ticked.'), stdout ())
-                   }else{
-                       write (sprintf ('Unlocked node dimensions successfully even if the check box is not ticked.'), stdout ())
-                   }
-               }else{
-                   write (sprintf ('Error in RCy3::lockNodeDimensions. Could not lock/unlocked node dimensions'), stderr ())
-               }
-               invisible(request.res)
-               
-           }) # lockNodeDimensions
 
 
