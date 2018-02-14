408caa366ba8d37c59740af7a02ccb84e8d02f4c
Alexander Pico <apico@gladstone.ucsf.edu>
2018-02-13 22:42:22 -0800

    4 new fxns for Collections
    Former-commit-id: d075195147eee9b10e8f0dae0fa2dc05e217e7a5 [formerly a0407221ed93cf648e3acbc958612fe7c19b4d28]
    Former-commit-id: 308fbd5a0be25610ff4f8db5b8917e732cb65261
    Former-commit-id: 202cbe0da7a49efa8f11b7bce346fd58ec12d780
    Former-commit-id: ec951331dbfb930e22ef76a1039492165c46e7b5

--- R/Collections.R
+++ R/Collections.R
@@ -1,6 +1,75 @@
 # ==============================================================================
 # Functions for getting information about network COLLECTIONS.
-#
 # ------------------------------------------------------------------------------
-#TODO getCollectionList(sub.title=NULL,base.url=.defaultBaseUrl) http://localhost:1234/v1/collections?subsuid=sub.suid
-#TODO getCollectionCount(base.url=.defaultBaseUrl) http://localhost:1234/v1/collections/count 
+#' @title Get Collection List
+#'
+#' @description FUNCTION_DESCRIPTION
+#' @param base.url DESCRIPTION
+#' @return RETURN_DESCRIPTION
+#' @examples \donttest{
+#' getCollectionList()
+#' }
+#' @export
+getCollectionList <- function(base.url=.defaultBaseUrl){
+    res <- cyrestGET('collections', base.url)
+    coll.names <- lapply(res, function(x) getCollectionName(x, base.url))
+    return(unlist(coll.names))
+}
+
+# ------------------------------------------------------------------------------
+#' @title Get Collection Suid
+#'
+#' @description FUNCTION_DESCRIPTION
+#' @param network DESCRIPTION
+#' @param base.url DESCRIPTION
+#' @return RETURN_DESCRIPTION
+#' @examples \donttest{
+#' getCollectionSuid()
+#' }
+#' @export
+getCollectionSuid <- function(network=NULL, base.url=.defaultBaseUrl){
+    net.suid <- getNetworkSuid(network, base.url)
+    res <- cyrestGET('collections',
+                     parameters = list(subsuid=net.suid),
+                     base.url = base.url)
+    return(res)
+}
+
+# ------------------------------------------------------------------------------
+#' @title Get Collection Name
+#'
+#' @description FUNCTION_DESCRIPTION
+#' @param collection.suid DESCRIPTION
+#' @param base.url DESCRIPTION
+#' @return RETURN_DESCRIPTION
+#' @examples \donttest{
+#' getCollectionName()
+#' }
+#' @export
+getCollectionName <- function(collection.suid=NULL, base.url=.defaultBaseUrl){
+    if(is.null(collection.suid))
+        collection.suid <- getCollectionSuid()
+    res <- cyrestGET(paste('collections',collection.suid,'tables/default', sep="/"),
+                     base.url = base.url)
+    return(unname(unlist(res$rows)['name']))
+}
+
+
+# ------------------------------------------------------------------------------
+#' @title Get Collection Networks
+#'
+#' @description FUNCTION_DESCRIPTION
+#' @param collection.suid DESCRIPTION
+#' @param base.url DESCRIPTION
+#' @return RETURN_DESCRIPTION
+#' @examples \donttest{
+#' getCollectionNetworks()
+#' }
+#' @export
+getCollectionNetworks <- function(collection.suid=NULL, base.url=.defaultBaseUrl){
+    if(is.null(collection.suid))
+        collection.suid <- getCollectionSuid()
+    res <- cyrestGET(paste('collections',collection.suid,'subnetworks', sep="/"),
+                     base.url = base.url)
+    return(res)
+}
