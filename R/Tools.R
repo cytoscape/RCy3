868b0835fa4affbe7f5d01bcae1aa3eb1b2899ab
Alexander Pico <apico@gladstone.ucsf.edu>
2018-02-18 17:44:31 -0800

    more docs
    Former-commit-id: d622d7941a529b1e4b52d54e3de422bf756f5239 [formerly 3a43170de7fb6cb1c573b035db7023d1dec38ec4]
    Former-commit-id: a9db857aa99fe0ddc241a0307a7f7ccdaec0ad7b
    Former-commit-id: c5004a256f046fa05b58aa7ab50a06649a6a63bb
    Former-commit-id: 8aa36c2a587bf5d29552293f2a1e53fd21366633

--- R/Tools.R
+++ R/Tools.R
@@ -1,3 +1,6 @@
+# ==============================================================================
+# Functions related to TOOLS found in the Tools Menu in Cytoscape.
+# 
 # ------ TODO -----------------------------------------------------------------
 #http://localhost:1234/diffusion/v1/52/views/772/diffuse
 #diffuse(title=NA, view=NA)
@@ -8,64 +11,3 @@
 #cyBrowserShow(id=NA) 
 #cyBrowserVersion(obj) 
 
-# ------------------------------------------------------------------------------
-#' @title Map Identifiers
-#'
-#' @description FUNCTION_DESCRIPTION
-#' @param column Name of column containing identifiers of type specied by \code{map.from}.
-#' @param species Common name for species associated with identifiers, e.g., Human. 
-#' See details.
-#' @param map.from Type of identifier found in specified \code{column}. See details.
-#' @param map.to Type of identifier to populate in new column. See details.
-#' @param force.single (optional) Whether to return only first result in cases
-#' of one-to-many mappings; otherwise the new column will hold lists of identifiers.
-#' Default is TRUE.
-#' @param table (optional) Name of table, e.g., node (default), edge or network
-#' @param namespace (optional) Namespace of table, e.g., default (default), shared
-#' or hidden
-#' @param network DESCRIPTION
-#' @param base.url DESCRIPTION
-#' @details Supported species: Human, Mouse, Rat, Frog, Zebrafish, Fruit fly,
-#' Mosquito, Worm, Arabidopsis thaliana, Yeast, E. coli, Tuberculosis.
-#' 
-#' Supported identifier types (depending on species): Ensembl, Entrez Gene, 
-#' Uniprot-TrEMBL, miRBase, UniGene,  HGNC (symbols), MGI, RGD, SGD, ZFIN, 
-#' FlyBase, WormBase, TAIR.
-#' @return dataframe with map.from and map.to columns. Beware: if map.to is not unique,
-#' it will be suffixed with an incrementing number in parentheses, e.g., if 
-#' mapIdentifiers is repeated on the same network. However, the original map.to 
-#' column will be returned regardless.
-#' @examples \donttest{
-#' mapped.cols <- mapIdentifiers('name','Yeast','Ensembl','SGD')
-#' #          name        SGD
-#' #17920  YER145C S000000947
-#' #17921  YMR058W S000004662
-#' #17922  YJL190C S000003726
-#' #...
-#' }
-#' @export
-mapIdentifiers <- function(column, species, map.from, map.to, force.single=TRUE,
-                           table='node', namespace='default',
-                           network=NULL, base.url=.defaultBaseUrl){
-    net.suid <- getNetworkSuid(network)
-    tbl <- paste(net.suid,namespace,table, sep=" ")
-    
-    if(force.single)
-        fs <- "true"
-    else
-        fs <- "false"
-    
-    all.cols <- getTableColumnNames(table, namespace, network, base.url)
-    if(!column %in% all.cols)
-        stop(sprintf("ERROR:mapIdentifiers, %s does not exist", column))
-    
-    commandsPOST(paste0('idmapper map column columnName="',column,
-                        '" forceSingle="',fs,
-                        '" mapFrom="',map.from,
-                        '" mapTo="',map.to,
-                        '" species="',species,
-                        '" table="',tbl,'"'))
-    
-    getTableColumns(table=table, columns=c(column,map.to), namespace = namespace, 
-                    network = network, base.url = base.url)
-}
