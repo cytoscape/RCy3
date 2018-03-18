066a8480591f4d6cf5832c83f88e905be65452d1
Alexander Pico <apico@gladstone.ucsf.edu>
2018-03-17 20:01:10 -0700

    0 errors | 0 warnings | 2 notes
    Former-commit-id: 6ba847e5a1bb12c32906e8db5ec6ec7586eab873 [formerly 00a9a3e2262124c6ab1b4b291ec3eb80fbe9df9d]
    Former-commit-id: a841e5586245e21c59f21944e82fdf8a587bb493
    Former-commit-id: ca4dbef908ec48cac91fbf4260521622b798482e
    Former-commit-id: f9d7f80c2ed44ce3b6b54ed197dd460e707cf62f

--- vignettes/pdf-vignettes.R
+++
@@ -1,7 +0,0 @@
-setwd("/git/RCy3/vignettes")
-require(knitr)
-require(markdown)
-
-knit("Overview.Rmd")
-markdownToHTML('Overview.md', 'Overview.html', options=c("use_xhml"))
-system("pandoc -s Overview.html -o Overview.pdf")
