c98445e8bbe0b124415bc627d4ba7b9c1cd6938e
Alexander Pico <alex.pico@gladstone.ucsf.edu>
2018-03-25 16:50:22 -0700

    removed completed todo note
    Former-commit-id: 4c1c27ebc8df74ee1affa3d58ec7ff2682ab7714 [formerly 8c81f4d19ffbb21d70e51675e9c2bc324139e3a6]
    Former-commit-id: 497451a8f7973b44880de0321c2d6ccff04cc558
    Former-commit-id: ca5858ae0e56b0cd6dbacbb3312c28ebe89a3170
    Former-commit-id: 22870ed33ee7eb334215ac10a4da7614d5232af3

--- R/Tools.R
+++ R/Tools.R
@@ -1,10 +1,6 @@
 # ==============================================================================
 # Functions related to TOOLS found in the Tools Menu in Cytoscape.
 # 
-# ------ TODO -----------------------------------------------------------------
-#http://localhost:1234/diffusion/v1/52/views/772/diffuse
-#diffuse(title=NA, view=NA)
-
 # ------------------------------------------------------------------------------
 #' @title Cybrowser Close
 #'
