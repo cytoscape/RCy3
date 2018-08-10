47e4049a3399bbc9df608cd3eb38655f04c30a0d
Alexander Pico <apico@gladstone.ucsf.edu>
2018-08-10 16:29:32 -0700

    bugfix: fixes #37

--- R/RCy3-utils.R
+++ R/RCy3-utils.R
@@ -31,7 +31,7 @@
         test <- sapply(node.names, function(x){x %in% dict[,'SUID']})
         if(all(test))  #provided SUIDs already!
             return(node.names)
-        node.SUIDs <- dict[which(dict$name  %in% node.names),'SUID']
+        node.SUIDs <- dict$SUID[match(node.names, dict$name)]
         return(node.SUIDs)
 }
 # ------------------------------------------------------------------------------
@@ -41,7 +41,7 @@
         test <- sapply(node.suids, function(x){x %in% dict[,'name']})
         if(all(test)) #provided names already!
             return(node.suids)
-        node.names <- dict[which(dict$SUID  %in% node.suids),'name']
+        node.names <- dict$name[match(node.suids, dict$SUID)]
         return(node.names)
 }
 
@@ -49,7 +49,7 @@
 # Replaces edge names with SUIDs.
 .edgeNameToEdgeSUID<-function(edge.names, network=NULL, base.url=.defaultBaseUrl) {
         dict <- getTableColumns('edge',c('SUID','name'),'default',network, base.url)
-        edge.SUIDs <- dict[which(dict$name  %in% edge.names),'SUID']
+        edge.SUIDs <- dict$SUID[match(edge.names, dict$name)]
         return(edge.SUIDs)
 }
 
@@ -57,7 +57,7 @@
 # Replaces edge SUIDs with names.
 .edgeSUIDToEdgeName<-function(edge.suids, network=NULL, base.url=.defaultBaseUrl) {
         dict <- getTableColumns('edge',c('SUID','name'),'default',network, base.url)
-        edge.names <- dict[which(dict$SUID  %in% edge.suids),'name']
+        edge.names <- dict$name[match(edge.suids, dict$SUID)]
         return(edge.names)
 }
 
