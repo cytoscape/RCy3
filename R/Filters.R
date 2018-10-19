7082729a0da824525b756b8fa2fb5d4c45005258
Alexander Pico <apico@gladstone.ucsf.edu>
2018-10-18 23:33:38 -0700

    new filter functions; NEWS; version bump

---
+++ R/Filters.R
@@ -0,0 +1,202 @@
+# ==============================================================================
+# Functions for working with FILTERS for the selection of nodes and edges in 
+# networks, including operations to import and export filters. In the Cytoscape
+# user interface, filters are managed in the Select tab of the Control Panel.
+#
+# ==============================================================================
+#' @title Apply Filter
+#'
+#' @description Run an existing filter by supplying the filter name.
+#' @param filter.name Name of filter to apply. Default is "Default filter".
+#' @param container Type of filter: "filter" (default) or "chain"
+#' @param network (optional) Name or SUID of the network. Default is the 
+#' "current" network active in Cytoscape.
+#' @param base.url (optional) Ignore unless you need to specify a custom domain,
+#' port or version to connect to the CyREST API. Default is http://localhost:1234
+#' and the latest version of the CyREST API supported by this version of RCy3.
+#' @return List of selected nodes and edges.
+#' @examples \donttest{
+#' applyFilter('myFilter')
+#' }
+#' @importFrom RJSONIO toJSON
+#' @export
+applyFilter<-function(filter.name="Default filter", container="filter", network=NULL, 
+                      base.url = .defaultBaseUrl){
+    net.SUID <- getNetworkSuid(network,base.url)
+    
+    cmd.container <- paste('container',container, sep='=')
+    cmd.name <- paste('name',filter.name,sep='=')
+    cmd.network <- paste('network=SUID',net.SUID, sep=':')
+    
+    commandsPOST(paste('filter apply',
+                       cmd.container,
+                       cmd.name,
+                       cmd.network, 
+                       sep=' '), base.url)
+    return(list(nodes=getSelectedNodes(), edges=getSelectedEdges()))
+}
+
+# ------------------------------------------------------------------------------
+#' @title Create Column Filter
+#'
+#' @description Creates a filter to control node or edge selection. Works on
+#' columns of boolean, string, numeric and lists. Note the unique restrictions
+#' for criterion and predicate depending on the type of column being filtered.
+#' @param filter.name Name for filter.
+#' @param column Table column to base filter upon.
+#' @param criterion  For boolean columns: TRUE or FALSE. For string columns: a 
+#' string value, e.g., "hello". If the predicate is REGEX then this can be a 
+#' regular expression as accepted by the Java Pattern class 
+#' (https://docs.oracle.com/javase/7/docs/api/java/util/regex/Pattern.html). For 
+#' numeric columns: If the predicate is BETWEEN or IS_NOT_BETWEEN then this is 
+#' a two-element vector of numbers, example: c(1,5), otherwise a single number.
+#' @param predicate  For boolean columns: IS, IS_NOT. For string columns: IS, 
+#' IS_NOT, CONTAINS, DOES_NOT_CONTAIN, REGEX. For numeric columns: IS, IS_NOT, 
+#' GREATER_THAN, GREATER_THAN_OR_EQUAL, LESS_THAN, LESS_THAN_OR_EQUAL, BETWEEN, 
+#' IS_NOT_BETWEEN
+#' @param caseSensitive (optional) If string matching should be case sensitive. 
+#' Default is FALSE.
+#' @param anyMatch (optional) Only applies to List columns. If true then at least 
+#' one element in the list must pass the filter, if false then all the elements 
+#' in the list must pass the filter. Default is TRUE.
+#' @param type (optional) Apply filter to "nodes" (default) or "edges".
+#' @param base.url (optional) Ignore unless you need to specify a custom domain,
+#' port or version to connect to the CyREST API. Default is http://localhost:1234
+#' and the latest version of the CyREST API supported by this version of RCy3.
+#' @return List of selected nodes and edges.
+#' @examples \donttest{
+#' createColumnFilter('myFilter', 'log2FC', c(-1,1), "IS_NOT_BETWEEN")
+#' createColumnFilter('myFilter', 'pValue', 0.05, "LESS_THAN")
+#' createColumnFilter('myFilter', 'function', "kinase", "CONTAINS", FALSE)
+#' createColumnFilter('myFilter', 'name', "^Y.*C$", "REGEX")
+#' createColumnFilter('myFilter', 'isTarget', TRUE , "IS")
+#' }
+#' @importFrom RJSONIO fromJSON
+#' @export
+createColumnFilter<-function(filter.name, column, criterion, predicate, 
+                             caseSensitive=FALSE, anyMatch=TRUE, 
+                             type="nodes", base.url = .defaultBaseUrl){
+    
+    col.type <- getTableColumnTypes(substr(type,1,4), base.url = base.url)[[column]]
+    
+    if(predicate %in% c("BETWEEN","IS_NOT_BETWEEN")){
+        if(!length(criterion)==2)
+            stop ("criterion must be a list of two numeric values, e.g., c(0.5,2.0)")
+    } else if (predicate %in% c("GREATER_THAN", "GREATER_THAN_OR_EQUAL")){
+        col.vals <- getTableColumns(substr(type,1,4), column, base.url = base.url)
+        crit.max <- max(na.omit(col.vals))
+        criterion <- c(criterion[1], crit.max)
+    } else if (predicate %in% c("LESS_THAN", "LESS_THAN_OR_EQUAL")){
+        col.vals <- getTableColumns(substr(type,1,4), column, base.url = base.url)
+        crit.max <- min(na.omit(col.vals))
+        criterion <- c(crit.max,criterion[1])
+    } else if (is.numeric(criterion[1]) & predicate == "IS"){
+        criterion <- c(criterion[1],criterion[1])
+        predicate <- "BETWEEN"
+    } else if (is.numeric(criterion[1]) & predicate == "IS_NOT"){
+        criterion <- c(criterion[1],criterion[1])
+        predicate <- "IS_NOT_BETWEEN"
+    }else if (is.logical(criterion[1]) & predicate == "IS_NOT"){
+        criterion <- !criterion
+    }
+    
+    cmd.name <- paste0('name="',filter.name,'"')
+    cmd.json <- list(id="ColumnFilter", parameters=list(criterion=criterion, 
+                                                        columnName=column,
+                                                        predicate=predicate,
+                                                        caseSensitive=caseSensitive,
+                                                        anyMatch=anyMatch,
+                                                        type=type))
+    cmd.body <- toJSON(list(name=filter.name, json=cmd.json))
+    
+    .postCreateFilter(cmd.body, base.url)
+ 
+    return(list(nodes=getSelectedNodes(), edges=getSelectedEdges()))
+}
+# ------------------------------------------------------------------------------
+#' @title Create Degree Filter
+#'
+#' @description Creates a filter to control node selection base on in/out degree.
+#' @param filter.name Name for filter.
+#' @param criterion  A two-element vector of numbers, example: c(1,5).
+#' @param predicate BETWEEN (default) or IS_NOT_BETWEEN
+#' @param edgeType (optional) Type of edges to consider in degree count: 
+#' ANY (default), UNDIRECTED, INCOMING, OUTGOING, DIRECTED
+#' @param base.url (optional) Ignore unless you need to specify a custom domain,
+#' port or version to connect to the CyREST API. Default is http://localhost:1234
+#' and the latest version of the CyREST API supported by this version of RCy3.
+#' @return List of selected nodes and edges.
+#' @examples \donttest{
+#' createDegreeFilter('myFilter', c(2,5))
+#' }
+#' @importFrom RJSONIO fromJSON
+#' @export
+createDegreeFilter<-function(filter.name, criterion, predicate="BETWEEN", 
+                            edgeType="ANY", base.url = .defaultBaseUrl){
+    
+    if(!length(criterion)==2)
+        stop ("criterion must be a list of two numeric values, e.g., c(2,5)")
+
+    cmd.name <- paste0('name="',filter.name,'"')
+    cmd.json <- list(id="DegreeFilter", parameters=list(criterion=criterion, 
+                                                        predicate=predicate,
+                                                        edgeType=edgeType))
+    cmd.body <- toJSON(list(name=filter.name, json=cmd.json))
+    
+    .postCreateFilter(cmd.body, base.url)
+    
+    return(list(nodes=getSelectedNodes(), edges=getSelectedEdges()))
+}
+
+# ------------------------------------------------------------------------------
+#' @title Export Filters
+#'
+#' @description Saves filters to file in JSON format.
+#' @param filename (char) Path and name of the filters file to save. Default is 
+#' "filters.json"
+#' @param base.url (optional) Ignore unless you need to specify a custom domain,
+#' port or version to connect to the CyREST API. Default is http://localhost:1234
+#' and the latest version of the CyREST API supported by this version of RCy3.
+#' @return Path to saved file
+#' @examples \donttest{
+#' exportFilters()
+#' }
+#' @export
+exportFilters<-function(filename = "filters.json", base.url = .defaultBaseUrl){
+    commandsGET(paste0('filter export file="',filename,'"'),base.url)
+}
+
+# ------------------------------------------------------------------------------
+#' @title Import Filters
+#'
+#' @description Loads filters from a file in JSON format.
+#' @param filename (char) Path and name of the filters file to load. 
+#' @param base.url (optional) Ignore unless you need to specify a custom domain,
+#' port or version to connect to the CyREST API. Default is http://localhost:1234
+#' and the latest version of the CyREST API supported by this version of RCy3.
+#' @return Path to saved file
+#' @examples \donttest{
+#' importFilters()
+#' }
+#' @export
+importFilters<-function(filename , base.url = .defaultBaseUrl){
+    commandsGET(paste0('filter import file="',filename,'"'),base.url)
+}
+
+# ------------------------------------------------------------------------------
+# Internal function to process special json list syntax with pesky single quotes. This
+# is an alternative to commandsPOST.
+#' @importFrom httr POST
+#' @importFrom httr content_type_json
+.postCreateFilter<-function(cmd.body, base.url){
+    cmd.url <- paste0(base.url, '/commands/filter/create')
+    cmd.body <- gsub("json\": {\n", "json\": \\'{\n", cmd.body, perl = TRUE)
+    cmd.body <- gsub("\n} \n} \n}", "\n} \n}\\' \n}", cmd.body, perl = TRUE)
+    
+    res=POST(url=cmd.url, body=cmd.body, encode="json", content_type_json())
+    if(res$status_code > 299){
+        write(sprintf("RCy3::.postCreateFilter, HTTP Error Code: %d\n url=%s\n body=%s", 
+                      res$status_code, URLencode(cmd.url), cmd.body), stderr())
+        stop(fromJSON(rawToChar(res$content))$errors[[1]]$message)
+    } 
+}
