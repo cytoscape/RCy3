836aa1e76e3b10fb8ba871bd4e6779fa283e8230
Yihang Xin <yxin@LAXUCR08173.local>
2021-02-03 02:43:23 -0800

    roadmap step 3

--- R/RCy3-notebook.R
+++ R/RCy3-notebook.R
@@ -1,4 +1,54 @@
 # ------------------------------------------------------------------------------
+#' @title spoofResponse
+#' @description class
+#' @import uchardet
+#' @import jsonlite
+#' @export
+spoofResponse <- setClass(
+    "spoofResponse",
+    slots = c(
+        selfURL = "ANY",
+        selfStatusCode = "ANY",
+        selfReason = "ANY",
+        selfText = "ANY"
+    )
+)
+
+setGeneric("repr", function(object, ...) standardGeneric("repr"))
+setGeneric("jsonLoads", function(object, ...) standardGeneric("jsonLoads"))
+setGeneric("raiseForStatus", function(object, ...) standardGeneric("raiseForStatus"))
+
+setMethod(f="initialize", signature="spoofResponse",
+          definition=function(.Object) {
+              .Object@selfURL <- "jupyter-bridge"
+              .Object@selfStatusCode <- "0"
+              .Object@selfReason <- "reason"
+              .Object@selfText <- "text"
+              return(.Object)
+          }
+)
+
+setMethod("repr", "spoofResponse", function(object, ...) {
+    dput(object@selfStatusCode)
+}) 
+
+setMethod("jsonLoads", "spoofResponse", function(object, ...) {
+    spoofResponsetext <- fromJSON(object@selfText)
+    return(spoofResponsetext)
+}) 
+
+setMethod("raiseForStatus", "spoofResponse", function(object, ...) {
+    if(object@selfStatusCode < 500 & object@selfStatusCode >= 400){
+        stop("Client Error")
+    }
+    else if(object@selfStatusCode < 600 & object@selfStatusCode >= 500){
+        stop("Server Error")
+    }
+}) 
+
+
+
+# ------------------------------------------------------------------------------
 #' @title getBrowserClientChannel
 #'
 #' @description Get the unique channel
@@ -57,13 +107,49 @@ getBrowserClientJs<-function(){
 #' @import httr
 #' @export
 doRequestRemote<-function(){
-    x <- list(command = "GET", url = "http://127.0.0.1:1234/v1/version")
-    http_request <- toJSON(x)
-    url_post <- sprintf('%s/queue_request?channel=%s',JupyterBRIDGEURL, CHANNEL)
-    POST(url_post, content_type_json(), body = http_request)
-    url_get <- sprintf('%s/dequeue_reply?channel=%s',JupyterBRIDGEURL, CHANNEL)
-    r1 <- GET(url_get)
-    r1 <- content(r1, "text")
-    return(r1)
+    tryCatch(
+        expr = {
+            x <- list(command = "GET", url = "http://127.0.0.1:1234/v1/version")
+            http_request <- toJSON(x)
+            url_post <- sprintf('%s/queue_request?channel=%s',JupyterBRIDGEURL, CHANNEL)
+            r <- POST(url_post, content_type_json(), body = http_request)
+            print(status_code(r))
+        },
+        error = function(e){
+            message('Error posting to Jupyter-bridge!')
+            print(e)
+        }
+    )
+    tryCatch(
+        expr = {
+            #while (TRUE){
+                url_get <- sprintf('%s/dequeue_reply?channel=%s',JupyterBRIDGEURL, CHANNEL)
+                r <- GET(url_get)
+                #if(status_code(r) != 408){break}
+            },
+        #},
+        error = function(e){
+            message('Error receiving from Jupyter-bridge!')
+            print(e)
+        }        
+    )
+    tryCatch(
+        expr = {
+            rContent <- content(r, "text")
+            encoding <- detect_str_enc(rContent)
+            message <- str(iconv(rContent, to=encoding))
+            cyReply <- fromJSON(message)
+            print(cyReply)
+        },
+        error = function(e){
+            message('Undeciperable message received from Jupyter-bridge!')
+            print(e)
+        }
+    )
+    r = spoofResponse()
+    r@selfStatusCode <- cyReply
+    r@selfReason <- cyReply
+    r@selfText <- cyReply
+    return(r)
 }
 # ------------------------------------------------------------------------------
