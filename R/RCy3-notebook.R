66ebeed71a0f355c4a63cfbda3d0c21b746d658e
Yihang Xin <yxin@LAXUCR08173.local>
2021-02-05 13:43:49 -0800

    roadmap 3 revise

--- R/RCy3-notebook.R
+++ R/RCy3-notebook.R
@@ -115,10 +115,10 @@ doRequestRemote<-function(method, qurl, qbody=NULL){
     }
     tryCatch(
         expr = {
-            request <- list(command = method, url = qurl, data = qbody)
+            request <- list(command = method, url = qurl, data = qbody )
             print(request)
             url_post <- sprintf('%s/queue_request?channel=%s',JupyterBRIDGEURL, CHANNEL)
-            r <- POST(url_post, body = request, encode="json", content_type_json())
+            r <- POST(url_post, body = request, encode="json", content_type_json(), add_headers("Content-Type" = "application/json"))
             print(status_code(r))
         },
         error = function(e){
