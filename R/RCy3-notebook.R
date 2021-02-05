159fe4e466ee982f0cf04e45f149e1bbdf99c8c9
Yihang Xin <yxin@LAXUCR08173.local>
2021-02-05 14:09:48 -0800

    roadmap 3 revise

--- R/RCy3-notebook.R
+++ R/RCy3-notebook.R
@@ -115,10 +115,10 @@ doRequestRemote<-function(method, qurl, qbody=NULL){
     }
     tryCatch(
         expr = {
-            request <- list(command = method, url = qurl, data = qbody, headers = list("Content-Type" = 'application/json', "Accept" = 'application/json' ) )
+            request <- list(command = method, url = qurl, data = qbody, headers = list('Content-Type' = 'application/json', 'Accept' = 'application/json'))
             print(request)
             url_post <- sprintf('%s/queue_request?channel=%s',JupyterBRIDGEURL, CHANNEL)
-            r <- POST(url_post, body = request, encode="json", content_type_json(), add_headers("Content-Type" = "application/json"))
+            r <- POST(url_post, body = request, encode="json", content_type_json(), add_headers('Content-Type' = 'application/json'))
             print(status_code(r))
         },
         error = function(e){
@@ -130,7 +130,7 @@ doRequestRemote<-function(method, qurl, qbody=NULL){
         expr = {
             while (TRUE){
                 url_get <- sprintf('%s/dequeue_reply?channel=%s',JupyterBRIDGEURL, CHANNEL)
-                r <- GET(url_get, accept_json())
+                r <- GET(url_get)
                 if(status_code(r) != 408){break}
             }
         },
