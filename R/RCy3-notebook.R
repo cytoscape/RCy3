44e9ebf88ee4b10dff1a759eedbfc852b9a64e19
Yihang Xin <yxin@LAXUCR08173.local>
2021-02-05 17:36:20 -0800

    roadmap 3 revise

--- R/RCy3-notebook.R
+++ R/RCy3-notebook.R
@@ -109,7 +109,7 @@ getBrowserClientJs<-function(){
 #' }
 #' @import httr
 #' @export
-doRequestRemote<-function (method, qurl, qbody = NULL, headers = NULL) {
+doRequestRemote<-function(method, qurl, qbody=NULL, headers=NULL){
     tryCatch(expr = {
         request <- list(command = method, url = qurl, data = qbody, 
                         headers = list(`Content-Type` = "application/json", 
