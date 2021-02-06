c89c28568b9f6bab978f6e497615c91c33cc02e9
Yihang Xin <yxin@LAXUCR08173.local>
2021-02-06 02:02:44 -0800

    debug

--- R/RCy3-notebook.R
+++ R/RCy3-notebook.R
@@ -112,7 +112,7 @@ getBrowserClientJs<-function(){
 doRequestRemote<-function(method, qurl, qbody=NULL, headers=NULL){
     tryCatch(
         expr = {
-            request <- list(command = method, url = qurl, data = qbody, headers=list("Content-Type" = "application/json", "Accept" = "application/json"))
+            request <- list(command = method, url = qurl, data = qbody, headers=list("Content-Type" = "application/json"))
             url_post <- sprintf('%s/queue_request?channel=%s',JupyterBRIDGEURL, CHANNEL)
             r <- POST(url_post, body = request, encode="json", content_type_json(), add_headers("Content-Type" = "application/json"))
         },
