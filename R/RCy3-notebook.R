11d8167ef7396a55cc819a2271647d74397b9214
Yihang Xin <yxin@LAXUCR08173.local>
2021-02-07 01:06:45 -0800

    roadmap step4

--- R/RCy3-notebook.R
+++ R/RCy3-notebook.R
@@ -215,17 +215,9 @@ checkRunningRemote<-function(){
                     .GlobalEnv$runningRemote <- FALSE
                 },
                 error = function(e){
-                    tryCatch(
-                        expr = {
                     doRequestRemote("GET", 'http://127.0.0.1:1234/v1')
                     .GlobalEnv$runningRemote <- TRUE
-                        },
-                    error = function(e){
-                        message('Error initially contacting Jupyter-bridge!')
-                        print(e)
                         }
-                    )
-                }
             )
         }
     }else{
