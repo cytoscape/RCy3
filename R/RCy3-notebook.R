8bc4dd476aa6b32eb7cd0fc34aa791d087c261d4
Yihang Xin <yxin@LAXUCR08173.local>
2021-02-09 22:55:12 -0800

    step 5

--- R/RCy3-notebook.R
+++ R/RCy3-notebook.R
@@ -110,7 +110,7 @@ getBrowserClientJs<-function(){
 doRequestRemote<-function(method, qurl, qbody=NULL, headers=NULL){
     tryCatch(
         expr = {
-            request <- list(command = method, url = qurl, data = qbody, headers=list("Content-Type" = "application/json", "Accept" = "application/json"))
+            request <- list(command = method, url = qurl, data = qbody)#, headers=list("Content-Type" = "application/json", "Accept" = "application/json"))
             url_post <- sprintf('%s/queue_request?channel=%s',JupyterBRIDGEURL, CHANNEL)
             r <- POST(url_post, body = request)#, encode="json", content_type_json(), add_headers("Content-Type" = "application/json"))
         },
@@ -123,7 +123,7 @@ doRequestRemote<-function(method, qurl, qbody=NULL, headers=NULL){
         expr = {
             while (TRUE){
                 url_get <- sprintf('%s/dequeue_reply?channel=%s',JupyterBRIDGEURL, CHANNEL)
-                r <- GET(url_get, accept_json())
+                r <- GET(url_get)#, accept_json())
                 if(status_code(r) != 408){break}
             }
         },
