a204745bafc78b69b6dac901adc709b45c34937d
Yihang Xin <yxin@LAXUCR08173.local>
2021-02-09 22:06:42 -0800

    step 5

--- R/RCy3-notebook.R
+++ R/RCy3-notebook.R
@@ -123,7 +123,7 @@ doRequestRemote<-function(method, qurl, qbody=NULL, headers=NULL){
         expr = {
             while (TRUE){
                 url_get <- sprintf('%s/dequeue_reply?channel=%s',JupyterBRIDGEURL, CHANNEL)
-                r <- GET(url_get, accept_json())
+                r <- GET(URLencode(url_get))
                 if(status_code(r) != 408){break}
             }
         },
