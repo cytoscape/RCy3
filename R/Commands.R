f530cdeea36d71e9bc7a4846b06595a56a3a2d19
Yihang Xin <yxin@LAXUCR08173.local>
2021-02-09 17:38:13 -0800

    step 5

--- R/Commands.R
+++ R/Commands.R
@@ -143,7 +143,7 @@ cyrestPOST <- function(operation, parameters=NULL, body=NULL, base.url=.defaultB
     q.body <- body
     res <- NULL
     tryCatch(
-        res <- doRequest("POST", q.url, q.body), 
+        res <- doRequestWrapper("POST", q.url, q.body), 
         error=function(c) .cyError(c, res),
         warnings=function(c) .cyWarnings(c, res),
         finally=.cyFinally(res)
