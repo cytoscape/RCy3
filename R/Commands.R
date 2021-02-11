4c8f38cf32485b9f19ff95470c223ec3100d4327
Yihang Xin <yxin@LAXUCR08173.local>
2021-02-11 00:38:21 -0800

    step 5

--- R/Commands.R
+++ R/Commands.R
@@ -120,7 +120,7 @@ cyrestGET <- function(operation=NULL, parameters=NULL, base.url=.defaultBaseUrl)
     }
     } else{
         res <- doRequestRemote("GET", q.url)
-        return(fromJSON(rawToChar(res$content))$text)
+        return(res)
         }
 }
 
