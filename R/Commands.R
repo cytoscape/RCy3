082c850074ca7b5bd340568e024fe9dc7d29c5ca
Yihang Xin <yxin@LAXUCR08173.local>
2021-02-18 00:59:13 -0800

    test

--- R/Commands.R
+++ R/Commands.R
@@ -345,7 +345,7 @@ commandsGET<-function(cmd.string, base.url = .defaultBaseUrl){
     } else {
         q.url <- .command2getQuery(cmd.string, 'http://127.0.0.1:1234/v1')
         res <- doRequestRemote("GET", q.url)
-        res.html = rawToChar(res$content)
+        res.html = res$content
         return(res.html)
     }
 }
