9d4b5470d29d6fd583194bbe40644a1382dba976
Yihang Xin <yxin@LAXUCR08173.local>
2021-02-18 01:27:27 -0800

    test

--- R/RCy3-notebook.R
+++ R/RCy3-notebook.R
@@ -113,7 +113,6 @@ doRequestRemote<-function(method, qurl, qbody=NULL, headers=NULL){
             request <- list(command = method, url = qurl, data = qbody, headers=list("Content-Type" = "application/json", "Accept" = "application/json"))
             url_post <- sprintf('%s/queue_request?channel=%s',JupyterBRIDGEURL, CHANNEL)
             r <- POST(url_post, body = request, encode="json", content_type_json(), add_headers("Content-Type" = "application/json"))
-            print(names(r))
         },
         error = function(e){
             message('Error posting to Jupyter-bridge!')
@@ -125,7 +124,6 @@ doRequestRemote<-function(method, qurl, qbody=NULL, headers=NULL){
             while (TRUE){
                 url_get <- sprintf('%s/dequeue_reply?channel=%s',JupyterBRIDGEURL, CHANNEL)
                 r <- GET(url_get, accept_json())
-                print(names(r))
                 if(status_code(r) != 408){break}
             }
         },
@@ -153,7 +151,7 @@ doRequestRemote<-function(method, qurl, qbody=NULL, headers=NULL){
     jsonMessage@status_code <- cyReply[1]
     jsonMessage@Reason <- cyReply[2]
     jsonMessage@Text <- cyReply[3]
-    print(names(r))
+    print(rawToChar(r$content))
     return(r)
 }
 # ------------------------------------------------------------------------------
