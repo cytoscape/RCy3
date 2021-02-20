cf97dbab79bea7be987d41c3cfa296daa4bce10f
Yihang Xin <yxin@LAXUCR08173.local>
2021-02-19 22:07:59 -0800

    test

--- R/Commands.R
+++ R/Commands.R
@@ -462,6 +462,8 @@ commandsPOST<-function(cmd.string, base.url = .defaultBaseUrl){
     } else {
         post.url = .command2postQueryUrl(cmd.string, 'http://127.0.0.1:1234/v1')
         post.body = .command2postQueryBody(cmd.string)
+        print(post.url)
+        print(post.body)
         res <- doRequestRemote("POST", URLencode(post.url), post.body, headers=list("Content-Type" = "application/json", "Accept" = "application/json"))
         print(names(res))
         return(rawToChar(res$content))
