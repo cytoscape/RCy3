03fab29b8d07e4107585c6728a33f68919bebf52
Yihang Xin <yxin@LAXUCR08173.local>
2021-02-19 21:54:42 -0800

    test

--- R/Commands.R
+++ R/Commands.R
@@ -438,6 +438,7 @@ commandsHelp<-function(cmd.string='help', base.url = .defaultBaseUrl){
 #' @importFrom httr POST content_type_json
 #' @export
 commandsPOST<-function(cmd.string, base.url = .defaultBaseUrl){
+    if(!findRemoteCytoscape()){
     post.url = .command2postQueryUrl(cmd.string,base.url)
     post.body = .command2postQueryBody(cmd.string)
     tryCatch(
@@ -455,7 +456,21 @@ commandsPOST<-function(cmd.string, base.url = .defaultBaseUrl){
         }
     }else {
         invisible(res)
-        
+    }
+    } else {
+        post.url = .command2postQueryUrl(cmd.string, 'http://127.0.0.1:1234/v1')
+        post.body = .command2postQueryBody(cmd.string)
+        res <- doRequestRemote("POST", URLencode(post.url), post.body, headers=list("Content-Type" = "application/json", "Accept" = "application/json"))
+        if(length(res$content)>0){
+            res.data = fromJSON(rawToChar(res$content))$data
+            if(length(res.data)>0){
+                return(res.data)
+            } else{
+                invisible(res.data)
+            }
+        }else {
+            invisible(res)
+        }
     }
 }
 # ------------------------------------------------------------------------------
