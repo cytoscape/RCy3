ba9eb752887e433112395e7474d782242e7af845
Yihang Xin <yxin@LAXUCR08173.local>
2021-02-19 20:31:39 -0800

    test

--- R/Commands.R
+++ R/Commands.R
@@ -179,7 +179,7 @@ cyrestPOST <- function(operation, parameters=NULL, body=NULL, base.url=.defaultB
     }
     q.body <- toJSON(body)
     tryCatch(
-        res <- POST(url=URLencode(q.url), body=q.body, encode="json", content_type_json(), headers=list("Content-Type" = "application/json", "Accept" = "application/json")), 
+        res <- POST(url=URLencode(q.url), body=q.body, encode="json", content_type_json()), 
         error=function(c) .cyError(c, res),
         warnings=function(c) .cyWarnings(c, res),
         finally=.cyFinally(res)
@@ -262,7 +262,7 @@ cyrestPUT <- function(operation, parameters=NULL, body=FALSE, base.url=.defaultB
             q.url <- paste(q.url, q.params, sep="?")
         }
         q.body <- body
-        res <- doRequestRemote("PUT", URLencode(q.url), q.body)
+        res <- doRequestRemote("PUT", URLencode(q.url), q.body, headers=list("Content-Type" = "application/json", "Accept" = "application/json"))
         if(length(res$content)>0){
             res.char <- rawToChar(res$content)
             if (isValidJSON(res.char, asText = TRUE)){
