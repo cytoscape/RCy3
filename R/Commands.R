4f64bb47d7c94a7c0856b2e615856fbafb4a6c40
Yihang Xin <yxin@LAXUCR08173.local>
2021-02-24 13:08:21 -0800

    test

--- R/Commands.R
+++ R/Commands.R
@@ -463,16 +463,7 @@ commandsPOST<-function(cmd.string, base.url = .defaultBaseUrl){
         post.body = .command2postQueryBody(cmd.string)
         post.body = fromJSON(post.body)
         res <- doRequestRemote("POST", post.url, post.body, headers=list("Content-Type" = "application/json", "Accept" = "application/json"))
-        if(length(res$content)>0){
-            res.data = fromJSON(rawToChar(res$content))$data
-            if(length(res.data)>0){
-                return(res.data)
-            } else{
-                invisible(res.data)
-            }
-        }else {
-            invisible(res)
-        }
+        return(fromJSON(rawToChar(res$content))$data)
     }
 }
 # ------------------------------------------------------------------------------
