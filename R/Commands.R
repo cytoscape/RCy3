1b11e79af619cfbd979ca35a9e1569ffebee9678
Yihang Xin <yxin@LAXUCR08173.local>
2021-03-02 00:23:56 -0800

    test

--- R/Commands.R
+++ R/Commands.R
@@ -266,7 +266,7 @@ cyrestPUT <- function(operation, parameters=NULL, body=NULL, base.url=.defaultBa
         if(length(res$content)>0){
             res.char <- rawToChar(res$content)
             if (isValidJSON(res.char, asText = TRUE)){
-                return(fromJSON(fromJSON(res.char)$text))
+                return(fromJSON(res.char))
             } else {
                 return(res.char)
             }
