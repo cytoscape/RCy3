58350fb611e51c9c111f8c3ec3897da88362179b
Yihang Xin <yxin@LAXUCR08173.local>
2021-03-01 17:13:49 -0800

    test

--- R/Commands.R
+++ R/Commands.R
@@ -409,8 +409,7 @@ commandsHelp<-function(cmd.string='help', base.url = .defaultBaseUrl){
         res.html = htmlParse(rawToChar(res$content), asText=TRUE)
         res.elem = xpathSApply(res.html, "//p", xmlValue)
         res.list = (fromJSON(res.elem))$text
-        print(res.list)
-        
+        print(length(res.elem))
         if (length(res.elem)==1){
             res.list = fromJSON(unlist(strsplit(res.elem[1],"\n\\s*")))
         }
