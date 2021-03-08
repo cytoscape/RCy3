09ba9cec3fbfeefe69c70e6761d6e13a5eae5b25
Yihang Xin <yxin@LAXUCR08173.local>
2021-03-07 21:50:11 -0800

    Fix NDEX

--- R/Commands.R
+++ R/Commands.R
@@ -204,8 +204,6 @@ cyrestPOST <- function(operation, parameters=NULL, body=NULL, base.url=.defaultB
             q.url <- paste(q.url, q.params, sep="?")
         }
         q.body <- body
-        print(q.url)
-        print(q.body)
         res <- doRequestRemote("POST", URLencode(q.url), q.body, headers=list("Content-Type" = "application/json"))
         if(length(res$content)>0){
             res.char <- rawToChar(res$content)
