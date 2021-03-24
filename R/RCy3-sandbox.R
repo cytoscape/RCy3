f15cd4b1d1b4c362993f631f91efb397c314e740
Yihang Xin <yihang.xin@gladstone.ucsf.edu>
2021-03-24 01:26:23 -0700

    implement sandbox

--- R/RCy3-sandbox.R
+++ R/RCy3-sandbox.R
@@ -1,4 +1,4 @@
-defaultSandbox = list() # Once a sandbox is explicitly defined, it'll override this default
+defaultSandbox <- list() # Once a sandbox is explicitly defined, it'll override this default
 defaultSandboxPath <- NULL
 predefinedSandboxName <- 'default_sandbox'
 currentSandboxName <- NULL
@@ -10,11 +10,12 @@ sandboxTemplate <- list('sandboxName' = NULL,  'copySamples' = TRUE, 'reinitiali
 # ------------------------------------------------------------------------------
 #' @title sandboxInitializer
 #'
-#' @description sandboxInitializer.
+#' @description sandboxInitializer
 #' @return sandbox
 #' @examples \donttest{
 #' sandboxInitializer()
 #' }
+#' @export
 sandboxInitializer <- function(newSandbox=NULL, ...){
     if (length(newSandbox) == 1 && !is.null(newSandbox[['init']])){
         params <- newSandbox[['init']]
@@ -24,4 +25,143 @@ sandboxInitializer <- function(newSandbox=NULL, ...){
     sandbox <- sandboxTemplate
     return(sandbox)
 }
+
+# ------------------------------------------------------------------------------
+#' @title setDefaultSandbox
+#'
+#' @description setDefaultSandbox
+#' @return default sandbox
+#' @examples \donttest{
+#' setDefaultSandbox()
+#' }
+#' @export
+setDefaultSandbox <- function(newSandbox=NULL, ...){
+    .GlobalEnv$defaultSandbox <- sandboxInitializer(init=newSandbox)
+    return(.GlobalEnv$defaultSandbox)
+}
+
+# ------------------------------------------------------------------------------
+#' @title getDefaultSandbox
+#'
+#' @description getDefaultSandbox
+#' @return default sandbox
+#' @examples \donttest{
+#' getDefaultSandbox()
+#' }
+#' @export
+getDefaultSandbox <- function(){
+    return(.GlobalEnv$defaultSandbox)
+}
+
+# ------------------------------------------------------------------------------
+#' @title getCurrentSandboxName
+#'
+#' @description getCurrentSandboxName
+#' @return current sandbox name
+#' @examples \donttest{
+#' getCurrentSandboxName()
+#' }
+#' @export
+getCurrentSandboxName <- function(){
+    return(.GlobalEnv$currentSandboxName)
+}
+
+# ------------------------------------------------------------------------------
+#' @title getCurrentSandboxPath
+#'
+#' @description getCurrentSandboxPath
+#' @return current sandbox path
+#' @examples \donttest{
+#' getCurrentSandboxPath()
+#' }
+#' @export
+getCurrentSandboxPath <- function(){
+    return(.GlobalEnv$currentSandboxPath)
+}
+
+# ------------------------------------------------------------------------------
+#' @title getCurrentSandbox
+#'
+#' @description getCurrentSandbox
+#' @return current sandbox
+#' @examples \donttest{
+#' getCurrentSandbox()
+#' }
+#' @export
+getCurrentSandbox <- function(){
+    currentSandbox <- list("currentSandboxName" = .GlobalEnv$currentSandboxName, "currentSandboxPath" = .GlobalEnv$currentSandboxPath)
+    return(currentSandbox)
+}
+
+# ------------------------------------------------------------------------------
+#' @title setCurrentSandbox
+#'
+#' @description setCurrentSandbox
+#' @return current sandbox
+#' @examples \donttest{
+#' setCurrentSandbox()
+#' }
+#' @export
+setCurrentSandbox <- function(sandboxName, sandboxPath){
+    .GlobalEnv$currentSandboxName <- sandboxName
+    .GlobalEnv$currentSandboxPath <- sandboxPath
+    return(getCurrentSandbox())
+}
+
+# ------------------------------------------------------------------------------
+#' @title setSandboxReinitialize
+#'
+#' @description setSandboxReinitialize
+#' @return sandbox reinitialize
+#' @examples \donttest{
+#' setCurrentSandbox()
+#' }
+#' @export
+setSandboxReinitialize <- function(doReinitialize=TRUE){
+    .GlobalEnv$sandboxReinitialize <- doReinitialize
+    return(.GlobalEnv$sandboxReinitialize)
+}
+
+# ------------------------------------------------------------------------------
+#' @title getSandboxReinitialize
+#'
+#' @description getSandboxReinitialize
+#' @return sandbox reinitialize
+#' @examples \donttest{
+#' getSandboxReinitialize()
+#' }
+#' @export
+getSandboxReinitialize <- function(){
+    return(.GlobalEnv$sandboxReinitialize)
+}
+
+# ------------------------------------------------------------------------------
+#' @title getAbsSandboxPath
+#'
+#' @description getAbsSandboxPath
+#' @return file location
+#' @examples \donttest{
+#' getAbsSandboxPath()
+#' }
+#' @export
+getAbsSandboxPath <- function(){
+    return(NULL)
+}
+
+# ------------------------------------------------------------------------------
+#' @title resetDefaultSandbox
+#'
+#' @description resetDefaultSandbox
+#' @return NULL
+#' @examples \donttest{
+#' resetDefaultSandbox()
+#' }
+#' @export
+resetDefaultSandbox <- function(){
+    .GlobalEnv$defaultSandbox <- list()
+    .GlobalEnv$defaultSandboxPath <- NULL
+    setCurrentSandbox(NULL, NULL)
+    .GlobalEnv$sandboxReinitialize <- TRUE
+}
+
 # ------------------------------------------------------------------------------
