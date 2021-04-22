87774c36dc4455df434868b39b5d0d818d5cc239
Yihang Xin <yihang.xin@gladstone.ucsf.edu>
2021-04-21 23:25:54 -0700

    fix sandboxsendto

--- R/Sandbox.R
+++ R/Sandbox.R
@@ -123,7 +123,7 @@ sandboxSendTo <- function(sourceFile, destFile=NULL, overwrite=TRUE, sandboxName
         head <- dirname(sourceFile)
         destFile <- basename(sourceFile)
     }
-    res <- sandboxOp(glue('filetransfer toSandbox fileByteCount={finfo$size} overwrite={overwrite} fileBase64={fileContent64}'), sandboxName, fileName=destFile, base.url=base.url)
+    res <- sandboxOp(glue('filetransfer toSandbox fileByteCount={finfo$size} overwrite={overwrite} fileBase64="{fileContent64}"'), sandboxName, fileName=destFile, base.url=base.url)
     return(res)
 }
 
