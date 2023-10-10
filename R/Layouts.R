71a671f493fa4d8b258fd07bbc890445e561d19d
yihangx <yihang.xin@gladstone.ucsf.edu>
2023-10-10 01:38:29 -0700

    delete symbol in layout functions

--- R/Layouts.R
+++ R/Layouts.R
@@ -313,7 +313,7 @@ setLayoutProperties <- function (layout.name, properties.list, base.url = .defau
 rotateLayout <- function (angle, network = NULL, selected.only = FALSE, base.url = .defaultBaseUrl) {
     .verifySupportedVersions(cytoscape=3.11, base.url=base.url)
     suid <- getNetworkSuid(network,base.url)
-    res <- commandsPOST(paste0('layout rotate angle=',angle,' network="SUID:',suid,'", selectedOnly=',selected.only,''),base.url=base.url)
+    res <- commandsPOST(paste0('layout rotate angle=',angle,' network="SUID:',suid,'" selectedOnly=',selected.only,''),base.url=base.url)
     invisible(res)
 }
 
@@ -339,7 +339,7 @@ rotateLayout <- function (angle, network = NULL, selected.only = FALSE, base.url
 scaleLayout <- function (axis, scaleFactor, network = NULL, selected.only = FALSE, base.url = .defaultBaseUrl) {
     .verifySupportedVersions(cytoscape=3.11, base.url=base.url)
     suid <- getNetworkSuid(network,base.url)
-    res <- commandsPOST(paste0('layout scale axis=',axis,' scaleFactor=',scaleFactor,' network="SUID:',suid,'", selectedOnly=',selected.only,''),base.url=base.url)
+    res <- commandsPOST(paste0('layout scale axis=',axis,' scaleFactor=',scaleFactor,' network="SUID:',suid,'" selectedOnly=',selected.only,''),base.url=base.url)
     invisible(res)
 }
 
